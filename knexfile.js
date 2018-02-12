ec76d4af4e0410d9cff8246e91aeb28f42d2c586
Pat Neel <patrick.neel@colorado.edu>
2018-02-12 10:43:06 -0700

    added test set up

--- knexfile.js
+++ knexfile.js
@@ -9,5 +9,16 @@ module.exports = {
       directory: './db/seeds/dev'
     },
     useNullAsDefault: true
+  },
+  test: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/garagebin_test',
+    useNullAsDefualt: true,
+    migrations: {
+      directory: 'db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/test'
+    }
   }
 };
