d11bbc417d6bea4c1c0d285e5e912646d1bec2a3
Pat Neel <patrick.neel@colorado.edu>
2018-02-12 09:40:46 -0700

    seeded migrations

--- knexfile.js
+++ knexfile.js
@@ -1,34 +1,10 @@
-
 module.exports = {
-
   development: {
     client: 'pg',
     connection: 'postgres://localhost/garagebin',
-    useNullAsDefualt: true,
     migrations: {
       directory: './db/migrations'
     },
-    seeds: {
-      directory: './db/seeds/dev'
-    }
-  },
-  production: {
-    client: 'pg',
-    connection: `${process.env.DATABASE_URL}?ssl=true`,
-    migrations: {
-      directory: './db/migrations'
-    },
-      useNullAsDefault: true
-    },
-  test: {
-    client: 'pg',
-    connection: 'postgres://localhost/garagebin_test',
-    useNullAsDefualt: true,
-    migrations: {
-      directory: __dirname + 'db/migrations'
-    },
-    seeds: {
-      directory: './db/seeds/test'
-    }
+    useNullAsDefault: true
   }
 };
