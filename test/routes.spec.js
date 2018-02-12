2ffbe5830db90f72ade1722777b3426a6cc524bd
Pat Neel <patrick.neel@colorado.edu>
2018-02-12 11:06:04 -0700

    added basic client side tests

---
+++ test/routes.spec.js
@@ -0,0 +1,35 @@
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const server = require('../server');
+const knex = require('../db/knex.js');
+
+chai.use(chaiHttp);
+
+describe('Client Side Routes', () => {
+
+  it('should return the homepage', () => {
+    return chai.request(server)
+    .get('/')
+    .then(response => {
+      response.should.have.status(200);
+      response.should.be.html;
+    })
+    .catch(error => {
+      throw error;
+    })
+  })
+
+  it('should return a 404 if the route does not exist', () => {
+    return chai.request(server)
+    .get('/sadddd')
+    .then(() => {
+    })
+    .catch(error => {
+      response.should.have.status(404); 
+    })
+  })
+
+})
--- tests/routes.spec.js
+++
@@ -1,9 +0,0 @@
-process.env.NODE_ENV = 'test';
-
-const chai = require('chai');
-const should = chai.should();
-const chaiHttp = require('chai-http');
-const server = require('../server');
-const knex = require('../db/knex.js');
-
-chai.use(chaiHttp);
