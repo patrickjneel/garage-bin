60b1e330631351fedfe69d029d262e0e8d919308
Pat Neel <patrick.neel@colorado.edu>
2018-02-12 13:36:22 -0700

    working on toggling the door

--- public/scripts.js
+++ public/scripts.js
@@ -3,23 +3,45 @@ const showItems = async () => {
   const itemJson = await itemData.json()
   const items = itemJson.items
 
-  $('.item-list').addClass('clicked').empty()
 
   if ($('.item-list').addClass('clicked')) {
+  $('.item-list').addClass('clicked').empty()
   const shownItems = items.map(item => {
   $('.item-list').append(
     `<div class="item-cards">
-      <h4>${item.itemName}</h4>
-      <span>${item.itemReason}</span>
-      <span>${item.itemCleanliness}</span>
+      <h4>ITEM NAME:${item.itemName}</h4>
+      <span>REASON:${item.itemReason}</span>
+      <span>CLEANLINESS${item.itemCleanliness}</span>
     </div>
     `)
   }) 
     return shownItems  
-  } else {
-    $('.item-list').removeClass('clicked')
+  } 
+  if(!$('.item-list').addClass('clicked')) {
+    $('.item-list').removeClass('clicked').empty()
   }
+}
+
+const postItem = async (event) => {
+  const itemName = $('#item-name').val()
+  const itemReason = $('#item-reason').val()
+  const itemCleanliness = $('select').closest('option').val()
+  console.log(itemCleanliness)
+
+  // const postItem = await fetch('/api/v1/all_items', {
+  //   method: 'POST',
+  //   body: JSON.stringify({itemName, itemReason, itemCleanliness}),
+  //   headers: {
+  //     'Content-Type': 'application/json'
+  //   }
+  // })
+  // const itemData = await postItem.json()
+
+  // return itemData
+  $('#item-name').val('')
+  $('#item-reason').val('')
 }
 
 
 $('.show-btn').on('click', showItems)
+$('.add-item-btn').on('click', postItem)
