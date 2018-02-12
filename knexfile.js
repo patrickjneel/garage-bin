048e8f04a534af45e8b50032bdb6b9bcfa2350f5
Pat Neel <patrick.neel@colorado.edu>
2018-02-12 09:29:36 -0700

    added migrations

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,34 @@
-// Update with your config settings.
 
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: 'pg',
+    connection: 'postgres://localhost/garagebin',
+    useNullAsDefualt: true,
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
     }
   },
-
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: 'pg',
+    connection: `${process.env.DATABASE_URL}?ssl=true`,
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './db/migrations'
+    },
+      useNullAsDefault: true
+    },
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/garagebin_test',
+    useNullAsDefualt: true,
+    migrations: {
+      directory: __dirname + 'db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/test'
     }
   }
-
 };
