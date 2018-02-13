b2f9f6f5a9a8d52d68bc8b41f8997e14694ed963
Pat Neel <patrick.neel@colorado.edu>
2018-02-12 17:19:04 -0700

    still working on drop down

--- public/scripts.js
+++ public/scripts.js
@@ -168,6 +168,7 @@ const itemCount = async () => {
 
 const updateClean = async (event) => {
   const optionValue = $(event.target).val()
+  
   const updateStatus = await fetch('/api/v1/all_items/:id', {
     method: 'PATCH',
     body: JSON.stringify({optionValue}),
